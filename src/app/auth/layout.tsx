1a01589c5ad110091d0d9adbd90f0a22505aa9e1
Mohammed Arham Khan <md.arhamkhan09@gmail.com>
2024-08-20 18:23:58 +0530

    chore: update branding

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -10,7 +10,7 @@ export default function Layout({ children }: LayoutProps) {
       <div className="container relative h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
         <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex">
           <div className="absolute inset-0 bg-zinc-900" />
-          <div className="relative z-20 flex items-center text-lg font-medium">Apps by Weblabs Studio</div>
+          <div className="relative z-20 flex items-center text-lg font-medium">Apps by Studio</div>
           <div className="relative z-20 mt-auto">
             <blockquote className="space-y-2">
               <p className="text-lg">
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ import "./globals.css"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "Core Admin - Weblabs Studio",
+  title: "Studio Admin",
   description: "",
 }
 
