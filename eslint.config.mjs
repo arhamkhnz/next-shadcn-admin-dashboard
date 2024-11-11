b493ed966b59a9c1b8580126359da213d9086ca9
Mohammed Arham Khan <md.arhamkhan09@gmail.com>
2024-11-11 17:29:10 +0530

    feat: remove Airbnb config & update ESLint to latest version with flat config and custom rules

---
+++ eslint.config.mjs
@@ -0,0 +1,87 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import pluginJs from "@eslint/js";
+import pluginImport from "eslint-plugin-import";
+import pluginReact from "eslint-plugin-react";
+import tailwind from "eslint-plugin-tailwindcss";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+const compat = new FlatCompat({
+  baseDirectory: import.meta.dirname,
+});
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { ignores: [".github/", ".husky/", "node_modules/", ".next/", "src/components/ui", "*.config.ts"] },
+  {
+    languageOptions: { globals: globals.browser },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    plugins: {
+      import: pluginImport,
+      tailwindcss: tailwind,
+    },
+  },
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  ...tseslint.configs.recommended,
+  ...tailwind.configs["flat/recommended"],
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      // Custom Rules (Not covered by plugins)
+      "spaced-comment": ["error", "always", { exceptions: ["-", "+"] }],
+      "key-spacing": ["error", { beforeColon: false, afterColon: true }],
+      "no-useless-rename": "error",
+
+      // Import/Export Rules
+      "import/no-mutable-exports": "error",
+      "import/order": [
+        "error",
+        {
+          groups: ["builtin", "external", "internal", "parent", "sibling", "index"],
+          pathGroups: [
+            {
+              pattern: "react",
+              group: "external",
+              position: "before",
+            },
+            {
+              pattern: "{next,next/**}",
+              group: "external",
+              position: "before",
+            },
+          ],
+          pathGroupsExcludedImportTypes: [],
+          "newlines-between": "always",
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+        },
+      ],
+      "import/newline-after-import": "error",
+
+      // Whitespace and Punctuation (Style Rules)
+      "no-trailing-spaces": "error",
+      "no-multiple-empty-lines": ["error", { max: 1, maxEOF: 1 }],
+      "space-before-function-paren": ["error", "never"],
+      "space-in-parens": ["error", "never"],
+      "array-bracket-spacing": ["error", "never"],
+      "object-curly-spacing": ["error", "always"],
+      "func-call-spacing": ["error", "never"],
+      "computed-property-spacing": ["error", "never"],
+
+      // Naming Conventions
+      "no-underscore-dangle": ["error", { allow: ["_id", "__dirname"] }],
+
+      // Unused Variables
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": ["warn"],
+    },
+  },
+];
