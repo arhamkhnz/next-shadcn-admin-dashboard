30dd61f7ab21de3ef590c5e540dee6a96ddc8f66
arhamkhnz <md.arhamkhan09@gmail.com>
2025-07-11 19:28:30 +0530

    chore: temporarily disable middleware to avoid unnecessary edge function executions

--- src/middleware.disabled.ts
+++ src/middleware.disabled.ts
@@ -1,13 +1,15 @@
+// ⚠️ This middleware has been temporarily disabled to avoid unnecessary edge function executions.
+// To re-enable, rename this file to `middleware.ts`.
 import { NextRequest, NextResponse } from "next/server";
 
 import { authMiddleware } from "./middleware/auth-middleware";
 
 export function middleware(req: NextRequest) {
   // authMiddleware
-  // const response = authMiddleware(req)
-  // if (response) {
-  //   return response
-  // }
+  const response = authMiddleware(req);
+  if (response) {
+    return response;
+  }
 
   return NextResponse.next();
 }
