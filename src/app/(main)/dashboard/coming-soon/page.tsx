ab53bd81100b09913004689608f0be7dfcff2206
arhamkhnz <md.arhamkhan09@gmail.com>
2025-09-05 20:39:42 +0530

    chore: add coming soon page & update deps

---
+++ src/app/(main)/dashboard/coming-soon/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div className="flex h-full flex-col items-center justify-center space-y-2 text-center">
+      <h1 className="text-2xl font-semibold">Page not found.</h1>
+      <p className="text-muted-foreground">This page is under development and will be available in future updates.</p>
+    </div>
+  );
+}
