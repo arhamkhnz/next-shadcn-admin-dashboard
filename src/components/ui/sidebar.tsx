33c1cf9c6eca5e38eda3e3a093648fdc996139ce
Mohammed Arham Khan <md.arhamkhan09@gmail.com>
2025-10-18 23:46:19 +0530

    chore: update deps & shadcn component

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react"
 import { Slot as SlotPrimitive } from "radix-ui"
-import { cva, VariantProps } from "class-variance-authority"
+import { cva, type VariantProps } from "class-variance-authority"
 import { PanelLeftIcon } from "lucide-react"
 
 import { useIsMobile } from "@/hooks/use-mobile"
