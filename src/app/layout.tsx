809bdd628e2096c8b128a5406bbe145c9f25d83e
Mohammed Arham Khan <md.arhamkhan09@gmail.com>
2025-11-30 00:12:15 +0530

    chore: remove analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,6 @@ import { ReactNode } from "react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
-import { Analytics } from "@vercel/analytics/next";
-
 import { Toaster } from "@/components/ui/sonner";
 import { APP_CONFIG } from "@/config/app-config";
 import { getPreference } from "@/server/server-actions";
@@ -33,7 +31,6 @@ export default async function RootLayout({ children }: Readonly<{ children: Reac
           {children}
           <Toaster />
         </PreferencesStoreProvider>
-        <Analytics />
       </body>
     </html>
   );
