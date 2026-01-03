e1499dd529ff0dc245bc941bb1a371c90371c3d7
Mohammed Arham Khan <md.arhamkhan09@gmail.com>
2026-01-03 16:26:21 +0530

    chore: enable preload for all fonts

--- src/lib/fonts/registry.ts
+++ src/lib/fonts/registry.ts
@@ -16,77 +16,65 @@ import {
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-inter",
-  preload: true,
 });
 
 const jakarta = Plus_Jakarta_Sans({
   subsets: ["latin"],
   variable: "--font-jakarta",
-  preload: false,
 });
 
 const roboto = Roboto({
   subsets: ["latin"],
   weight: ["400", "500", "700"],
   variable: "--font-roboto",
-  preload: false,
 });
 
 const geist = Geist({
   subsets: ["latin"],
   variable: "--font-geist",
-  preload: false,
 });
 
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["400", "500", "600"],
   variable: "--font-poppins",
-  preload: false,
 });
 
 const outfit = Outfit({
   subsets: ["latin"],
   variable: "--font-outfit",
-  preload: false,
 });
 
 const geistMono = Geist_Mono({
   subsets: ["latin"],
   variable: "--font-geist-mono",
-  preload: false,
 });
 
 const dmSans = DM_Sans({
   subsets: ["latin"],
   variable: "--font-dm-sans",
-  preload: false,
 });
 
 const nunito = Nunito({
   subsets: ["latin"],
   variable: "--font-nunito",
-  preload: false,
 });
 
 const gabriela = Gabriela({
   weight: "400",
   subsets: ["latin"],
   variable: "--font-gabriela",
-  preload: false,
 });
 
 const manrope = Manrope({
   subsets: ["latin"],
   variable: "--font-manrope",
-  preload: false,
 });
 
 const greatVibes = Great_Vibes({
   subsets: ["latin"],
   weight: "400",
   variable: "--font-great-vibes",
-  preload: false,
 });
 
 export const fontRegistry = {
