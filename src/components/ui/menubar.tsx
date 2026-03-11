b2fe6c4b50d9cdf561ee0b65fb149d11fc6a6040
Mohammed Arham Khan <md.arhamkhan09@gmail.com>
2026-03-11 13:55:20 +0530

    chore: update shadcn components

--- src/components/ui/menubar.tsx
+++ src/components/ui/menubar.tsx
@@ -14,7 +14,7 @@ function Menubar({
     <MenubarPrimitive.Root
       data-slot="menubar"
       className={cn(
-        "flex h-9 items-center gap-1 rounded-md border bg-background p-1 shadow-xs",
+        "flex h-9 items-center gap-1 rounded-md border p-1 shadow-xs",
         className
       )}
       {...props}
