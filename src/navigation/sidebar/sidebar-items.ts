503e42c02f1a3846beb891d01973de0642928790
Mohammed Arham Khan <md.arhamkhan09@gmail.com>
2026-05-18 16:28:28 +0530

    feat: add invoice route

--- src/navigation/sidebar/sidebar-items.ts
+++ src/navigation/sidebar/sidebar-items.ts
@@ -122,9 +122,8 @@ export const sidebarItems: NavGroup[] = [
       },
       {
         title: "Invoice",
-        url: "/dashboard/coming-soon",
+        url: "/dashboard/invoice",
         icon: ReceiptText,
-        comingSoon: true,
       },
       {
         title: "Users",
